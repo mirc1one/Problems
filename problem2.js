032f53256529b1ea33257970db3c1913043b564b
Mircea Bratu <44461660+mirc1one@users.noreply.github.com>
2020-03-02 11:19:06 +0200

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,28 @@
+/**
+ * This problem was recently asked by Amazon:
+ *
+ * Given an integer, reverse the digits. Do not convert the integer into a string and reverse it.
+ * 
+ * Here's some examples and some starter code.
+ *
+ * print(reverse_integer(135))
+ * 531
+ *
+ * print(reverse_integer(-321))
+ * -123
+ */
+
+var integer = -551
+var reversedInteger = 0
+const isNegative = num => num < 0
+
+while ( integer !== 0 ) {
+    reversedInteger = ( reversedInteger * 10 ) + ( integer % 10 )
+    integer = parseInt( integer / 10 )
+}
+
+if ( isNegative( integer ) ) {
+    reversedInteger = -1 * reversedInteger
+}
+
+console.log(reversedInteger)
